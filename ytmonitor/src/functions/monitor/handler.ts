2d18228878683023d0294379a7ba6b604c8b630e
Edvin Dzidic <dzidic@tugraz.student.at>
2021-10-27 19:50:13 +0200

    Add functions for querying channels, playlists, videos and videoData, implement function that uses pagination to allow all data of those specific functions to be queried, update monitor mock json and schema

--- ytmonitor/src/functions/monitor/handler.ts
+++ ytmonitor/src/functions/monitor/handler.ts
@@ -2,13 +2,17 @@ import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 import schema from './schema';
-import { getChannels } from '../../services/youTube';
+import { getChannels, getPlaylists, getVideos, getAllYoutubeData, getVideoData } from '../../services/youTube';
 
 const monitor: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-    const channels = await getChannels(event.body.username);
-    return formatJSONResponse({
-        channels
-    });
+  const channels = await getAllYoutubeData(getChannels, event.body.username)
+  const playlists = await getAllYoutubeData(getPlaylists, event.body.channelId);
+  const videos = await getAllYoutubeData(getVideos, event.body.playlistId);
+  const videoData = (await getVideoData(videos[0].contentDetails.videoId)).data;
+  return formatJSONResponse({
+    channels
+  });
 }
 
 export const main = middyfy(monitor);
+
--- ytmonitor/src/services/youTube.ts
+++ ytmonitor/src/services/youTube.ts
@@ -2,10 +2,88 @@ import { executeHttp } from './apiClient';
 const baseUrl = process.env.YOUTUBE_API_URL;
 const apiKey = process.env.YOUTUBE_API_KEY;
 
-export const getChannels = async (username:string) =>{
-    const url = `${baseUrl}/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername=${username}&key=${apiKey}`;
-    const requestParams = {
-        url
+type DataBatch = {
+  nextData: any[];
+  nextPageToken: string;
+};
+
+type RequestData = {
+  status: number;
+  data: {
+    nextPageToken?: string | null;
+    items?: any[];
+  }
+}
+
+const getNextDataBatch = async (
+  queryData: Promise<RequestData>
+) => {
+  let batch: DataBatch = {
+    nextData: [],
+    nextPageToken: ""
+  };
+
+  let request = await queryData;
+
+  const { data } = request;
+
+  if (request.status === 200) {
+    batch.nextData = data?.items ?? [];
+    batch.nextPageToken = data?.nextPageToken;
+  }
+
+  return batch;
+};
+
+export const getAllYoutubeData = async (dataFunction: (id: string, token?: string) => Promise<RequestData>, id: string) => {
+  let nextToken: string | undefined;
+  const data = [];
+
+  while (true) {
+    const { nextData, nextPageToken } = await getNextDataBatch(dataFunction(id, nextToken));
+
+    data.push(...nextData);
+
+    if (!nextPageToken) {
+      break;
     }
-    return await executeHttp('get',requestParams);
+
+    nextToken = nextPageToken;
+  }
+
+  return data;
+}
+
+const withPageToken = (url: string, token?: string) => token ? url + `&pageToken=${token}` : url;
+
+export const getChannels = async (username:string, nextPageToken?: string) => {
+  const url = `${baseUrl}/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername=${username}&key=${apiKey}`;
+  const requestParams = {
+    url: withPageToken(url, nextPageToken)
+  }
+  return await executeHttp('get', requestParams);
+}
+
+export const getPlaylists = async (channelId:string, nextPageToken?: string) => {
+  const url = `${baseUrl}/playlists?part=snippet%2CcontentDetails&channelId=${channelId}&maxResults=25&key=${apiKey}`;
+  const requestParams = {
+    url: withPageToken(url, nextPageToken)
+  }
+  return await executeHttp('get', requestParams);
+}
+
+export const getVideos = async (playlistId:string, nextPageToken?: string) => {
+  const url = `${baseUrl}/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=${playlistId}&key=${apiKey}`;
+  const requestParams = {
+    url: withPageToken(url, nextPageToken)
+  }
+  return await executeHttp('get', requestParams);
+}
+
+export const getVideoData = async (videoId: string) => {
+  const url = `${baseUrl}/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${apiKey}`;
+  const requestParams = {
+    url
+  }
+  return await executeHttp('get', requestParams);
 }
