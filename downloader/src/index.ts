7fbff06f5d95d83da8a7aac3d19c6c84d90a2a92
demirselmanovic <demir@tryleverage.ai>
2021-10-29 16:59:24 +0200

    add downloader service as docker

--- downloader/src/index.ts
+++ downloader/src/index.ts
@@ -1,5 +1,17 @@
 import youtubedl from 'youtube-dl-exec';
 import { uploadFile } from './aws';
+import {start} from "repl";
+
+const getFileName = (output: string):string => {
+    const startMarker = '[download] Destination:';
+    const endMarker = '[download]';
+    const startInd = output.indexOf(startMarker) + startMarker.length;
+    const endInd = output.indexOf(endMarker, startInd);
+    if (0 < startInd && startInd < endInd){
+        return output.substring(startInd, endInd).trim();
+    }
+    return '';
+}
 
 const executeDownload = () => {
     console.log('ARGUMENTS:', process.argv);
@@ -20,8 +32,9 @@ const executeDownload = () => {
 //     youtubeSkipDashManifest: true,
 //     referer: 'https://www.youtube.com/'
     }).then(async output => {
-        console.log(output);
-        await uploadFile('FILE_NAME_HERE');
+        console.log(output.toString());
+        const fileName = getFileName(output.toString());
+        await uploadFile(fileName);
     })
 }
 
