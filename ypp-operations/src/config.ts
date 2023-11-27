587f896157836e7a61c2be88c6547ca872c52599
Zeeshan Akram <97m.zeeshan@gmail.com>
2023-11-27 18:18:59 +0500

    update ypp rewards params

--- ypp-operations/src/config.ts
+++ ypp-operations/src/config.ts
@@ -12,7 +12,7 @@ function readEnvInt<T>(key: string, defaultValue: T): number | T {
   }
 }
 
-function readEnvString<T>(key: string, defaultValue?: string): string {
+function readEnvString(key: string, defaultValue?: string): string {
   const value = process.env[key]
   if (value) {
     return value
@@ -31,14 +31,14 @@ let conf = {
   HUBSPOT_API_KEY: '',
 
   BRONZE_TIER_SIGNUP_REWARD_IN_USD: 2,
-  SILVER_TIER_SIGNUP_REWARD_IN_USD: 25,
-  GOLD_TIER_SIGNUP_REWARD_IN_USD: 50,
-  DIAMOND_TIER_SIGNUP_REWARD_IN_USD: 100,
+  SILVER_TIER_SIGNUP_REWARD_IN_USD: 50,
+  GOLD_TIER_SIGNUP_REWARD_IN_USD: 100,
+  DIAMOND_TIER_SIGNUP_REWARD_IN_USD: 200,
 
   BRONZE_TIER_REFERRAL_REWARD_IN_USD: 1,
-  SILVER_TIER_REFERRAL_REWARD_IN_USD: 12.5,
-  GOLD_TIER_REFERRAL_REWARD_IN_USD: 25,
-  DIAMOND_TIER_REFERRAL_REWARD_IN_USD: 50,
+  SILVER_TIER_REFERRAL_REWARD_IN_USD: 25,
+  GOLD_TIER_REFERRAL_REWARD_IN_USD: 50,
+  DIAMOND_TIER_REFERRAL_REWARD_IN_USD: 100,
 
   BRONZE_TIER_SYNC_REWARD_IN_USD: 0,
   SILVER_TIER_SYNC_REWARD_IN_USD: 1,
