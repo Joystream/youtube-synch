251224e21a197c06567531fe8af5fa24e1a0d66a
Zeeshan Akram <97m.zeeshan@gmail.com>
2023-11-27 18:23:36 +0500

    YPP-ops: update dynamodb queries

--- ypp-operations/src/dynamodb.ts
+++ ypp-operations/src/dynamodb.ts
@@ -4,48 +4,58 @@ import { YtChannel } from './types'
 
 const documentClient = new AWS.DynamoDB.DocumentClient()
 
-export async function countVideosSyncedAfter(channelId: string, date: string): Promise<number> {
+export async function countVideosSyncedAfter(channelId: string, yppSignupDate: string, date: string): Promise<number> {
   const params = {
     TableName: 'videos',
-    IndexName: 'channelId-publishedAt-index',
-    KeyConditionExpression: '#channelId = :hashVal and #publishedAt > :dateVal',
-    FilterExpression: '#state = :stateVal',
+    IndexName: 'channelId-createdAt-index',
+    KeyConditionExpression: '#channelId = :channelIdVal and #createdAt > :createdAtAtVal',
+    FilterExpression: '#state = :stateVal and #publishedAt > :publishedAtAtVal and #duration > :durationVal',
     ExpressionAttributeNames: {
       '#channelId': 'channelId',
+      '#createdAt': 'createdAt',
       '#publishedAt': 'publishedAt',
+      '#duration': 'duration',
       '#state': 'state',
     },
     ExpressionAttributeValues: {
-      ':hashVal': channelId,
+      ':channelIdVal': channelId,
+      ':createdAtAtVal': date,
+      ':publishedAtAtVal': yppSignupDate,
+      ':durationVal': config('MIN_VIDEO_DURATION_IN_MINS') * 60,
       ':stateVal': 'UploadSucceeded',
-      ':dateVal': date,
     },
   }
 
-  const videos = await documentClient.query(params).promise()
-  const videosEligibleForReward =
-    videos.Items?.filter((video) => video.duration > config('MIN_VIDEO_DURATION_IN_MINS') * 60).length || 0
-  return videosEligibleForReward
+  const videosEligibleForReward = (await documentClient.query(params).promise()).Items || []
+  return videosEligibleForReward.length
 }
 
-export async function getAllReferredChannels(referrerId: number, date: string): Promise<YtChannel[]> {
-  const params = {
+export async function getAllReferredChannels(referrerId: number, date?: string): Promise<YtChannel[]> {
+  // Define basic query parameters
+  let params: AWS.DynamoDB.DocumentClient.QueryInput = {
     TableName: 'channels',
     IndexName: 'referrerChannelId-index',
     KeyConditionExpression: '#referrerChannelId = :referrerId',
-    FilterExpression: 'begins_with(#yppStatus, :status) AND #createdAt >= :date',
     ExpressionAttributeNames: {
       '#referrerChannelId': 'referrerChannelId',
       '#yppStatus': 'yppStatus',
-      '#createdAt': 'createdAt',
     },
     ExpressionAttributeValues: {
       ':referrerId': referrerId,
       ':status': 'Verified::',
-      ':date': date,
     },
   }
 
+  // Add date filter if date is provided
+  if (date) {
+    params.FilterExpression = 'begins_with(#yppStatus, :status) AND #createdAt >= :date'
+    params.ExpressionAttributeNames = { ...params.ExpressionAttributeNames, ...{ '#createdAt': 'createdAt' } }
+    params.ExpressionAttributeValues = { ...params.ExpressionAttributeValues, ...{ ':date': date } }
+  } else {
+    params.FilterExpression = 'begins_with(#yppStatus, :status)'
+  }
+
+  // Execute the query
   const referredChannels = ((await documentClient.query(params).promise()).Items || []) as unknown as YtChannel[]
 
   return referredChannels
@@ -79,12 +89,22 @@ export async function getAllSuspendedChannels(): Promise<YtChannel[]> {
   return channels as YtChannel[]
 }
 
+/**
+ * @returns all YPP channels except with `Unverified` yppStatus
+ */
 export async function getAllChannels(): Promise<YtChannel[]> {
   const channels: YtChannel[] = []
   let cursor = undefined
   do {
     const params: AWS.DynamoDB.DocumentClient.ScanInput = {
       TableName: 'channels',
+      FilterExpression: '#yppStatus <> :yppStatusVal',
+      ExpressionAttributeNames: {
+        '#yppStatus': 'yppStatus',
+      },
+      ExpressionAttributeValues: {
+        ':yppStatusVal': 'Unverified',
+      },
       ExclusiveStartKey: cursor,
     }
 
