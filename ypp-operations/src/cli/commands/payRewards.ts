5d0e2f15975315a602dc7fc8580dd7e98e4e193b
Zeeshan Akram <97m.zeeshan@gmail.com>
2023-11-28 15:34:44 +0500

    [YPP-ops] fix: make channels paid in hubspot after the payment has been made

--- ypp-operations/src/cli/commands/payRewards.ts
+++ ypp-operations/src/cli/commands/payRewards.ts
@@ -3,24 +3,9 @@ import BN from 'bn.js'
 import chalk from 'chalk'
 import _ from 'lodash'
 import { getContactsToPay, updateYppContacts } from '../../hubspot'
-import { PayContactsInput, RevertPayingContactsInput } from '../../types'
+import { PayContactsInput } from '../../types'
 import DefaultCommandBase from '../base/default'
 
-let preventShutdown = false
-process.on('SIGINT', () => {
-  if (preventShutdown) {
-    console.log('Caught interrupt signal. Ignoring...')
-  } else {
-    process.exit(1)
-  }
-})
-
-/**
- * * The batch payment command/script is designed to be close to an atomic operation.
- * * It first update the contact's reward status in Hubspot to "Paid", then
- * * make the actual payment using directChannelPayment CLI command, if payment
- * * fails, it revert the contact's reward status in Hubspot to "To Pay"
- */
 export default class PayReward extends DefaultCommandBase {
   static description = `Pays reward to all Verified YPP channels for the latest reward cycle based on the calculated rewards in hubspot`
 
@@ -64,7 +49,7 @@ export default class PayReward extends DefaultCommandBase {
     const account = await this.getJoystreamMemberControllerAccount(payerMemberId)
 
     const channelsBatches = _.chunk(channels, batchSize)
-    for (const channelsBatch of channelsBatches) {
+    for (const [i, channelsBatch] of channelsBatches.entries()) {
       // prepare owed payment params
       const payments = channelsBatch.map((c) => {
         const signupReward = parseInt(c.sign_up_reward_in_usd || '0') / parseFloat(joyPrice)
@@ -73,12 +58,6 @@ export default class PayReward extends DefaultCommandBase {
         const totalJoyReward = signupReward + referralReward + syncReward
         return { channelId: c.gleev_channel_id, joyAmount: totalJoyReward }
       })
-
-      // Get nonce of the payer account (Getting nonce before updating the
-      // Hubspot contacts, because if done otherwise there might be a case
-      // that the Hubspot contacts are updated (i.e. set as "Paid") but then
-      // `getAccountNonce` operation errors, which we don't want.
-      const currentNonce = await this.getAccountNonce(account)
 
       // prepare params for updating contact's reward status fields in Hubspot
       const updateContactsRewardsInput: PayContactsInput = channelsBatch.map((c, i) => ({
@@ -89,45 +68,25 @@ export default class PayReward extends DefaultCommandBase {
           total_ypp_rewards: new BN(c.total_ypp_rewards || 0).addn(payments[i].joyAmount).toString(),
         },
       }))
-      // update contact's reward status in Hubspot to paid before making the payment
-      await updateYppContacts(updateContactsRewardsInput)
+
+      const currentNonce = await this.getAccountNonce(account)
 
       try {
         // make payments in batch transaction
-        preventShutdown = true
         await this.joystreamCli.directChannelPayment({
           payments: payments.map((p) => ({ ...p, amount: this.asHapi(p.joyAmount) })),
           rationale,
           payerMemberId,
         })
-      } catch (err) {
-        const newNonce = await this.getAccountNonce(account)
-        if (newNonce === currentNonce) {
-          // Means the payment transaction was not successful, so
-          // revert the contacts reward status in Hubspot to "To Pay"
-          const revertContactsRewardsInput: RevertPayingContactsInput = channelsBatch.map((c) => ({
-            id: c.contactId,
-            properties: {
-              latest_ypp_reward_status: 'To Pay',
-              sign_up_reward_in_usd: c.sign_up_reward_in_usd,
-              latest_referral_reward_in_usd: c.latest_referral_reward_in_usd,
-              videos_sync_reward: c.videos_sync_reward,
-              latest_ypp_reward: c.latest_ypp_reward,
-              total_ypp_rewards: c.total_ypp_rewards,
-            },
-          }))
-          await updateYppContacts(revertContactsRewardsInput)
-        }
-
-        // If the payment was cancelled by the user, then don't pay any more channels
-        if (err instanceof Error && err.message.includes('"exitSignal":"SIGINT"')) {
-          throw err
-        }
       } finally {
-        preventShutdown = false
+        const newNonce = await this.getAccountNonce(account)
+        if (newNonce === currentNonce + 1) {
+          // update contact's reward status in Hubspot to paid before making the payment
+          await updateYppContacts(updateContactsRewardsInput)
+
+          this.log(chalk.greenBright(`Done paying rewards to ${(i + 1) * channelsBatch.length} channels!`))
+        }
       }
     }
-
-    this.log(chalk.green(`Successfully paid rewards to YPP channels!`))
   }
 }
