cbb2d1d82943f33a93f7d53525126cf9168121be
Lezek123 <leszek@jsgenesis.com>
2024-11-28 14:56:57 +0100

    Fix polling: Retrieve missing video details, schedule channels based on changed videosNum

--- src/services/syncProcessing/YoutubePollingService.ts
+++ src/services/syncProcessing/YoutubePollingService.ts
@@ -7,10 +7,10 @@ import { YtChannel, YtVideo, verifiedVariants } from '../../types/youtube'
 import { LoggingService } from '../logging'
 import { JoystreamClient } from '../runtime/client'
 import { IYoutubeApi } from '../youtube/api'
+import { GaxiosError } from 'googleapis-common'
 
 export class YoutubePollingService {
   private logger: Logger
-  private lastPolledChannelId: number | undefined = undefined // Track the last successfully polled channel ID
 
   public constructor(
     logging: LoggingService,
@@ -46,33 +46,27 @@ export class YoutubePollingService {
     const sleepInterval = pollingIntervalMinutes * 60 * 1000
     while (true) {
       try {
-        const channels = _.orderBy(await this.performChannelsIngestion(), ['joystreamChannelId'], ['desc'])
-
-        console.log('Total channels to be polled:', channels.length)
-        // Start polling from the last successfully polled channel ID
-        const startIndex = this.lastPolledChannelId
-          ? channels.findIndex((channel) => channel.joystreamChannelId === this.lastPolledChannelId)
-          : 0
-        const channelsToPoll = channels.slice(startIndex)
+        const channelsWithNewVideos = _.orderBy(await this.performChannelsIngestion(), ['joystreamChannelId'], ['desc'])
 
         this.logger.info(
-          `Completed Channels Ingestion. Videos of ${channelsToPoll.length} channels will be prepared for syncing in this polling cycle....`
+          `Videos of ${channelsWithNewVideos.length} channels will be prepared for syncing in this polling cycle....`
         )
 
-        // Ingest videos of channels in a batch of 50 to limit IO/CPU resource consumption
-        const channelsBatch = _.chunk(channelsToPoll, 100)
+        // Ingest videos of channels in a batch of 100 to limit IO/CPU resource consumption
+        const channelsBatches = _.chunk(channelsWithNewVideos, 100)
         // Process each batch
 
-        let a = 0
-        for (const channels of channelsBatch) {
-          a = a + channels.length
-          await Promise.all(channels.map((channel) => this.performVideosIngestion(channel)))
-          this.lastPolledChannelId = channels[channels.length - 1].joystreamChannelId // Update last polled channel ID
-          console.log('videos ingestion batch:', a)
+        let channelsProcessed = 0
+        for (const channelsBatch of channelsBatches) {
+          // TODO: Uncomment after verification
+          // await Promise.all(channelsBatch.map((channel) => this.performVideosIngestion(channel)))
+          channelsProcessed += channelsBatch.length
+          this.logger.info(`Ingested videos of ${channelsProcessed}/${channelsWithNewVideos.length} channels...`)
         }
       } catch (err) {
         this.logger.error(`Critical Polling error`, { err })
       }
+      this.logger.info(`All videos ingested.`)
       this.logger.info(`Youtube polling service paused for ${pollingIntervalMinutes} minute(s).`)
       await sleep(sleepInterval)
       this.logger.info(`Resume polling....`)
@@ -84,8 +78,7 @@ export class YoutubePollingService {
    */
   private async performChannelsIngestion(): Promise<YtChannel[]> {
     // get all channels that need to be ingested
-    const channelsWithSyncEnabled = async () =>
-      await this.dynamodbService.repo.channels.scan(
+    const channelsWithSyncEnabled = await this.dynamodbService.repo.channels.scan(
         {},
         (scan) =>
           scan
@@ -101,22 +94,22 @@ export class YoutubePollingService {
             // * ingestion as we don't have access to their access/refresh tokens
             .where('performUnauthorizedSync')
             .eq(false)
-        // .and()
-        // .where('id')
-        // .eq('UCBWgpKlykQ8yPAEmTGdsXxw')
       )
 
-    // updated channel objects with uptodate info
-    const channelsToBeIngestedChunks = _.chunk(await channelsWithSyncEnabled(), 100)
+    this.logger.info(`Found ${channelsWithSyncEnabled.length} channels with sync enabled.`)
 
-    let a = 0
-    console.log('await channelsWithSyncEnabled()', (await channelsWithSyncEnabled()).length)
-    for (const channelsToBeIngested of channelsToBeIngestedChunks) {
-      a = a + channelsToBeIngested.length
-      console.log('channels ingestion batch:', a)
+    const channelsToBeIngestedChunks = _.chunk(channelsWithSyncEnabled, 50)
+    const channelsScheduledForVideoIngestion: YtChannel[] = []
+
+    let checkedChannelsCount = 0
+    let updatedChannelsCount = 0
+    for (const channelsBatch of channelsToBeIngestedChunks) {
+      const channelsStats = await this.youtubeApi.getChannelsStats(channelsBatch.map(c => c.id))
+      const channelStatsbyId = new Map(channelsStats.map(c => [c.id, c.statistics]))
+      checkedChannelsCount += channelsBatch.length
       const updatedChannels = (
         await Promise.all(
-          channelsToBeIngested.map(async (ch) => {
+          channelsBatch.map(async (ch) => {
             try {
               // ensure that Ypp collaborator member is still set as channel's collaborator
               const isCollaboratorSet = await this.joystreamClient.doesChannelHaveCollaborator(ch.joystreamChannelId)
@@ -132,26 +125,45 @@ export class YoutubePollingService {
                   lastActedAt: new Date(),
                 }
               }
-            } catch (err: unknown) {
-              if (err instanceof Error && err.message.includes('This account has been terminated')) {
-                this.logger.warn(
-                  `Opting out '${ch.id}' from YPP program as their Youtube channel has been terminated by the Youtube.`
-                )
-                return {
-                  ...ch,
-                  yppStatus: 'OptedOut',
-                  shouldBeIngested: false,
-                  lastActedAt: new Date(),
-                }
-              } else if (err instanceof Error && err.message.includes('The playlist does not exist')) {
-                this.logger.warn(`Opting out '${ch.id}' from YPP program as Channel does not exist on Youtube.`)
-                return {
-                  ...ch,
-                  yppStatus: 'OptedOut',
-                  shouldBeIngested: false,
-                  lastActedAt: new Date(),
-                }
+              // Check channel's updated stats
+              const channelStats = channelStatsbyId.get(ch.id)
+              if (!channelStats) {
+                this.logger.warn(`Missing channel stats for channel ${ch.id}! Verifying channel status...`)
+                // TODO: Uncomment after verification
+                // try {
+                //   await this.youtubeApi.getChannel({ id: ch.userId, accessToken: ch.userAccessToken, refreshToken: ch.userRefreshToken })
+                // } catch (e) {
+                //   if (e instanceof GaxiosError && e.message.includes('YouTube account of the authenticated user is suspended')) {
+                //     this.logger.warn(
+                //       `Opting out '${ch.id}' from YPP program as their Youtube channel has been terminated by the Youtube.`
+                //     )
+                //     return {
+                //       ...ch,
+                //       yppStatus: 'OptedOut',
+                //       shouldBeIngested: false,
+                //       lastActedAt: new Date(),
+                //     }
+                //   }
+                //   this.logger.warn(`Status of channel ${ch.id} unclear. Will be skipped for this cycle...`, { err: e })
+                //   return
+                // }
+                this.logger.warn(`Status of channel ${ch.id} unclear. Will be skipped for this cycle...`)
+                return
               }
+              // Schedule for video ingestion if videoCount has changed
+              if (channelStats.videoCount !== ch.statistics.videoCount) {
+                this.logger.debug('Channel stats changed', { oldStats: ch.statistics, newStats: channelStats })
+                channelsScheduledForVideoIngestion.push(ch)
+              }
+              // Update stats in DynamoDB
+              // TODO: Uncomment after verification
+              // return {
+              //   ...ch,
+              //   statistics: {
+              //     ...channelStats
+              //   }
+              // }
+            } catch (err: unknown) {
               this.logger.error('Failed to fetch updated channel info', { err, channelId: ch.joystreamChannelId })
             }
           })
@@ -159,13 +171,21 @@ export class YoutubePollingService {
       ).filter((ch): ch is YtChannel => ch !== undefined)
 
       // save updated  channels
-      await this.dynamodbService.repo.channels.upsertAll(updatedChannels)
+      // TODO: Uncomment after verification
+      // await this.dynamodbService.repo.channels.upsertAll(updatedChannels)
+      updatedChannelsCount += updatedChannels.length
 
+      this.logger.info(`Processed ${checkedChannelsCount}/${channelsWithSyncEnabled.length} channels...`)
       // A delay between batches if necessary to prevent rate limits or high CPU/IO usage
       await sleep(100)
     }
 
-    return channelsWithSyncEnabled()
+    this.logger.info(
+      `Finished channel ingestion. ` +
+      `Updated ${updatedChannelsCount} channels.` +
+      `Found ${channelsScheduledForVideoIngestion.length} channels with new videos.`
+    )
+    return channelsScheduledForVideoIngestion
   }
 
   public async performVideosIngestion(channel: YtChannel, initialIngestion = false) {
@@ -179,7 +199,7 @@ export class YoutubePollingService {
       // get all video Ids that are not yet being tracked
       let untrackedVideos = await this.getUntrackedVideos(channel, videos)
 
-      console.log('untrackedVideos.length', videos.length, untrackedVideos.length, channel.joystreamChannelId)
+      this.logger.debug(`Found ${untrackedVideos.length} untracked videos (out of last ${videos.length}) for channel ${channel.joystreamChannelId}`)
       // save all new videos to DB including
       await this.dynamodbService.repo.videos.upsertAll(untrackedVideos)
     } catch (err) {
@@ -201,6 +221,7 @@ export class YoutubePollingService {
           ytChannelId: channel.id,
         })
 
+        // TODO: Uncomment after verification
         // await this.dynamodbService.repo.channels.save({
         //   ...channel,
         //   yppStatus: 'OptedOut',
